a1e0f32453da7981b0e03930f51c3c05edc331d9
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2026-09-01 22:47:55 +0000

    refactor: sort imports in urls.py for consistency

--- amae/urls.py
+++ amae/urls.py
@@ -1,9 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
-
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import include, path
 from django.views.generic.base import RedirectView
 
 from missions.views import home
